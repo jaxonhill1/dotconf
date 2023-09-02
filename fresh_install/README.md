2c7acfc53c9090bd253756dd8f843677b84cfb5c
jaxonhill1 <jaxonhill19@gmail.com>
2023-09-01 23:50:38 -0500

    Added fresh_install directory

---
+++ fresh_install/README.md
@@ -0,0 +1,2 @@
+This directory contains scripts to be run on a fresh Linux install
+Intended to serve primarily as a package list
---
+++ fresh_install/for_Ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# atool is a wrapper utility, for all common archiving and compression tools.
+# for example: you can type 'aunpack archive.tar.gz' and atool will run the right command for you
+apt install atool
+
+# Helix is a more modern CLI text editor, written in Rust
+add-apt-repository ppa:maveonair/helix-editor
+apt update
+apt install helix
